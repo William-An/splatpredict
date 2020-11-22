2fbdf240a4c3cebdbe6656739e9cfa3b4ad097f6
William-An <China_Aisa@live.com>
2020-11-22 17:45:15 -0500

    Add support for continuous training

--- nbc.py
+++ nbc.py
@@ -16,16 +16,17 @@ class NBC:
             y0: ...
             y1: {
                 feature condition probability on y1
-                    {
-                        name: p1
-                        name2: p2
-                    }
+                total_size: 
+                feature_class_count:
+                name: count
+                name2: count
             }
         }
     }
     prior: {
-        y1: p,
-        y2: p
+        total_size:
+        y1: count,
+        y2: count
     }
     """
 
@@ -45,7 +46,7 @@ class NBC:
 
     def train(self, train_dataset=None, label_name="", train_data=None, train_label=None):
         """
-        All of the inputs are in pandas dataframe
+        Note: label name and feature name of the dataset cannot be `total_size` or `feature_class_count` as it is reserved
         train_dataset: training dataset combining data and label
         train_data: training data in pandas dataframe
         train_label: training label in pandas dataframe
@@ -62,34 +63,43 @@ class NBC:
             self.label_name = train_label.columns[0]
             train_dataset = pd.concat([train_data, train_label], axis=1)
 
-        # 1. Initialize conditional dictionary
+        # TODO Create customized class structure to store priors and conditional
+
+        # 1. Initialize conditional dictionary if first time running train()
         feature_names = self.feature_names
-        self.conditional = dict([(feature_name, {}) for feature_name in feature_names])
+        # If self.conditional is an empty dict
+        isFirst = not bool(self.conditional)
+        if isFirst:
+            self.conditional = dict([(feature_name, {}) for feature_name in feature_names])
 
         # 2. Initialize prior dictionary
         self.label_name = label_name
-        self.priors = dict.fromkeys(train_dataset[self.label_name].unique())
+        # If priors dict is empty
+        if isFirst:
+            self.priors = dict.fromkeys(train_dataset[self.label_name].unique(), 0)
+            self.priors["total_size"] = 0
 
-        # 3. Partition continuous variable
-        partition = self.model_params["partition"]
-        n = train_dataset.shape[0]
-        bins_count = int(np.ceil(n**0.5))
-        sample_data = train_dataset.sample(frac=self.model_params["sample_frac"])
-        for feature_name in feature_names:
-            unique_count = sample_data[feature_name].unique().size
-            # Partition feature if it is larger than threshold
-            if unique_count > self.model_params["max_discretize_count"] and self.model_params["continuous"].get(feature_name, False) == True:
-                if partition == "sqrt":
-                    train_dataset[feature_name] = pd.cut(train_dataset[feature_name], bins_count)
-                    self.bins_count = bins_count
-                elif partition == "qcut":
-                    train_dataset[feature_name] = pd.qcut(train_dataset[feature_name], q=self.model_params["q"], duplicates="drop")
-                else:
-                    raise ValueError("Invalid partition scheme for continuous variable")
+        # 3. Partition continuous variable if first time running train()
+        if isFirst:
+            partition = self.model_params["partition"]
+            n = train_dataset.shape[0]
+            bins_count = int(np.ceil(n**0.5))
+            sample_data = train_dataset.sample(frac=self.model_params["sample_frac"])
+            for feature_name in feature_names:
+                unique_count = sample_data[feature_name].unique().size
+                # Partition feature if it is larger than threshold
+                if unique_count > self.model_params["max_discretize_count"] and self.model_params["continuous"].get(feature_name, False) == True:
+                    if partition == "sqrt":
+                        train_dataset[feature_name] = pd.cut(train_dataset[feature_name], bins_count)
+                        self.bins_count = bins_count
+                    elif partition == "qcut":
+                        train_dataset[feature_name] = pd.qcut(train_dataset[feature_name], q=self.model_params["q"], duplicates="drop")
+                    else:
+                        raise ValueError("Invalid partition scheme for continuous variable")
 
-                # Get categories to store in order to identify
-                # which category the new value in predict is
-                self.categories[feature_name] = train_dataset[feature_name].cat.categories
+                    # Get categories to store in order to identify
+                    # which category the new value in predict is
+                    self.categories[feature_name] = train_dataset[feature_name].cat.categories
 
         # 4. Calculate conditional and prior
         # Prior
@@ -100,7 +110,8 @@ class NBC:
 
     def calculate_conditional(self, train_dataset, smoothing=True):
         label_name = self.label_name
-        label_values = self.priors.keys()
+        label_values = list(self.priors.keys())
+        label_values.remove("total_size")
         for feature in self.feature_names:
             # Calculate the probability conditioning on label value for each feature value
             # using value_counts()
@@ -115,7 +126,11 @@ class NBC:
             else:
                 feature_values = feature_label_pair[feature].unique()   # All possible value for this feature
 
-            feature_conditional = dict.fromkeys(label_values)
+            # Get the feature_conditional dict for the current feature
+            feature_conditional = self.conditional.get(feature)
+            # If the dict is empty, initialize a new one
+            if not bool(feature_conditional):
+                feature_conditional = {label: dict() for label in label_values}
             for label_value in label_values:
                 # Get feature-single_label
                 # pair such that (X, y=yi)
@@ -136,20 +151,35 @@ class NBC:
                 # If smoothing,
                 # P(X = xi | y = yj) = (count(xi and yj) + 1)/(count(yj) + count(feature_value))
 
+                # Store the count
+
                 # Count (X = xi and y = yj) and store in dict
+                total_size = feature_single_label.shape[0]  # total size of the feature-label pair
                 feature_count = feature_single_label[feature].value_counts().to_dict()
 
                 # Calculate freq
-                for val in feature_values:
-                    if smoothing:
-                        feature_count[val] = (feature_count.get(val, 0) + 1) / (N + k)
-                    else:
-                        feature_count[val] = feature_count.get(val, 0) / N
-                feature_conditional[label_value] = feature_count
+                # for val in feature_values:
+                #     if smoothing:
+                #         feature_count[val] = (feature_count.get(val, 0) + 1) / (N + k)
+                #     else:
+                #         feature_count[val] = feature_count.get(val, 0) / N
+
+                # todo label get same value update?
+                for feature_value in feature_values:
+                    feature_conditional[label_value][feature_value] = feature_conditional[label_value].get(feature_value, 0) + feature_count.get(feature_value, 0)
+                feature_conditional[label_value]["total_size"] = feature_conditional[label_value].get("total_size", 0) + total_size
+                if feature_conditional[label_value].get("feature_class_count", None) is None:
+                    feature_conditional[label_value]["feature_class_count"] = k
             self.conditional[feature] = feature_conditional
 
     def calculate_prior(self, train_dataset):
-        self.priors = train_dataset[self.label_name].value_counts(normalize=True).to_dict()
+        label_count = train_dataset[self.label_name].value_counts().to_dict()
+        total_size = 0
+        for label in label_count:
+            tmp_count = label_count[label]
+            self.priors[label] += tmp_count
+            total_size += tmp_count
+        self.priors["total_size"] += total_size
 
     def evaluate(self, test_dataset, label_name):
         test_data = test_dataset.drop(label_name, axis=1)
@@ -187,18 +217,20 @@ class NBC:
         :param data: single record in dict format
         :return: predict_label: predicted label
         """
+        # TODO Perform smoothing here
         predicted_prob = self.priors.copy()
+        predicted_prob.pop("total_size")
+        for label in predicted_prob.keys():
+            predicted_prob[label] /= self.priors["total_size"]
         for label in predicted_prob:
             for feature in data:
                 category = self.categories.get(feature, None)
                 value = data[feature]
-                if category is None:
-                    # No need to classify into category data
-                    try:
-                        predicted_prob[label] = predicted_prob[label] * self.conditional[feature][label][value]
-                    except KeyError:
-                        print(f"KeyError: feature: {feature}, label: {label}, value: {value}")
-                else:
+                value_cat_map = value   # Default to be the value, need to classify if it is categorical
+                probability_dict = self.conditional[feature][label]
+
+                total_size = probability_dict["total_size"]
+                if category is not None:
                     # Need to calculate the category of the data feature value
                     # TODO Handle if the value is already a category value
                     value_cat_map = None
@@ -221,7 +253,13 @@ class NBC:
                                 value_cat_map = category[category.size - 1]
                     except NotImplementedError:
                         value_cat_map = value
-                    predicted_prob[label] = predicted_prob[label] * self.conditional[feature][label][value_cat_map]
+                
+                # Compute prob
+                if self.model_params["smoothing"]:
+                    tmp_prob = (probability_dict[value_cat_map] + 1) / (total_size + probability_dict["feature_class_count"]) 
+                else:
+                    tmp_prob = (probability_dict[value_cat_map] / total_size) 
+                predicted_prob[label] = predicted_prob[label] * tmp_prob
 
         # Calculate actual probability
         sum = 0
